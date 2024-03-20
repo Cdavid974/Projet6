f6a69f55f688109121b0e4cfc849a5045916db68
David Cavanillas <cavanillas.david@gmail.com>
2024-03-20 15:22:17 +0400

    modified:   wp-content/uploads/elementor/css/post-39.css

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -5,3 +5,16 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 };
+// Fonction pour ajouter le lien "Admin" dans le menu
+function ajouter_lien_admin_dans_menu($items, $args) {
+    // Vérifier si l'utilisateur est connecté
+    if (is_user_logged_in()) {
+        // Construire le lien
+        $lien_admin = '<li class="menu-item"><a href="' . admin_url() . '">Admin</a></li>';
+        
+        // Ajouter le lien à la fin du menu
+        $items .= $lien_admin;
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin_dans_menu', 10, 2);
