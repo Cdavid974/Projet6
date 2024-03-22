317335014347523b1935724735312f51ba9cf087
David Cavanillas <cavanillas.david@gmail.com>
2024-03-22 18:29:26 +0400

    modified:   wp-content/themes/Planty/css/theme.css 	modified:   wp-content/themes/Planty/functions.php

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -6,12 +6,13 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 };
 // Fonction pour ajouter le lien "Admin" dans le menu
-function ajouter_lien_admin_dans_menu($items, $args) {
+function ajouter_lien_admin_dans_menu($items, $args)
+{
     // Vérifier si l'utilisateur est connecté
     if (is_user_logged_in()) {
         // Construire le lien
         $lien_admin = '<li class="menu-item"><a href="' . admin_url() . '">Admin</a></li>';
-        
+
         // Ajouter le lien à la fin du menu
         $items .= $lien_admin;
     }
